f519a8b432727103e44067c20eafaa0251cb6e9c
miloskrca96 <59830743+miloskrca96@users.noreply.github.com>
2022-06-18 18:32:03 +0200

    CSS and JS changes, eval removed, stack added
    The eval function has been removed, the stack is now used to evaluate the expression, the problem with the decimal point needs to be solved

--- js/main.js
+++ js/main.js
@@ -34,25 +34,121 @@ for(let i=0; i < options.childElementCount; i++) {
 
 
 let action = ''
+repetitionStack = []
 
 for(let i=0; i < buttons.length;i++) {
     buttons[i].addEventListener('click', (event) => {
         event.preventDefault();
 
         if(buttons[i].innerText == "=") {
-            resultSection.value = eval(resultSection.value)
+            let values = [] 
+            let ops = []
+            let tokens = resultSection.value;
             action = 'calculation-done'
+         
+            for(let i=0; i < tokens.length; i++) {
+                if(tokens[i] >= '0' && tokens[i] <= '9'){
+                    let val = 0;
+                     
+                    while(i < tokens.length && tokens[i] >= '0' && tokens[i] <= '9') {
+                        val = (val*10) + (tokens[i]-'0');
+                        i++;
+                    }
+                     
+                    values.push(val);
+                    i--;
+                } else {
+                    while(ops.length != 0 && precedence(ops[ops.length-1]) >= precedence(tokens[i])) {
+                        let val2 = values[values.length-1];
+                        values.pop();
+                        
+                        let val1 = values[values.length-1];
+                        values.pop();
+                        
+                        let op = ops[ops.length-1];
+                        ops.pop();
+                        
+                        values.push(applyOperation(val1, val2, op));
+                    }
+                    ops.push(tokens[i]);
+                }
+            }
+
+            while(ops.length != 0){
+                let val2 = values[values.length-1];
+                values.pop();
+                            
+                let val1 = values[values.length-1];
+                values.pop();
+                            
+                let op = ops[ops.length-1];
+                ops.pop();
+                            
+                values.push(applyOperation(val1, val2, op));
+            }
+
+            let result = values[values.length-1];
+            resultSection.value = result;
+          
         } else if(buttons[i].innerText == "Reset") {
             resultSection.value = ""
         } else if(buttons[i].innerText == "Del") {
             if(action == 'inserting') {
                 resultSection.value = resultSection.value.substr(0, resultSection.value.length-1);
+                repetitionStack.pop();
             } else {
                 resultSection.value = ""
             }
         } else {
-            resultSection.value += buttons[i].innerText;
-            action = 'inserting'
+            if(buttons[i].innerText == '-' || buttons[i].innerText == '*' || buttons[i].innerText == '+' || buttons[i].innerText == '/' || buttons[i].innerText == '.') {
+                if(repetitionStack.length == 0) {
+                    addToRepetitionStack(buttons[i].innerText)
+                    action = 'inserting'
+                } else {
+                    let topElem = repetitionStack[repetitionStack.length - 1]
+
+                    if(topElem != buttons[i].innerText && notSign(topElem)) {
+                        addToRepetitionStack(buttons[i].innerText)
+                        action = 'inserting'
+                    }
+                }
+            } else {
+                addToRepetitionStack(buttons[i].innerText)
+                action = 'inserting'
+            } 
         }
     })
+}
+
+
+//HELPER FUNCTIONS
+
+function precedence(op){
+    if(op == '+'||op == '-')
+        return 1;
+    if(op == '*'||op == '/')
+        return 2;
+    return 0;
+}
+
+function applyOperation(a, b, op){
+    switch(op){
+        case '+': return a + b;
+        case '-': return a - b;
+        case '*': return a * b;
+        case '/': return a / b;
+    }
+}
+
+function notSign(elem) {
+    if(elem == '.' || elem == '+' || elem == '/' || elem == '-' || elem == '*') {
+        return false
+    }
+
+    return true
+}
+
+function addToRepetitionStack(val) {
+    resultSection.value += val;
+    repetitionStack.push(val)
 }
